755d06d46bb2b17808d81e2a96905eb604770db1
Aleksandr Knyazev <knyaleks@yandex.ru>
2023-02-16 10:33:02 +0300

    Lesson_1 Math.abs

--- Lesson_1/program.java
+++ Lesson_1/program.java
@@ -10,7 +10,7 @@ public class program {
     public static void main (String args[]) {
         try (Scanner in = new Scanner(System.in)){
           System.out.print("Введите целое положительное число :  ");
-          int n = in.nextInt();
+          int n = Math.abs (in.nextInt());
           int triang = n * (n + 1) / 2;
           System.out.print(triang);
         }
